50e7b658e047a9aabe98f9a3f97dc61417d42687
kowalskikoduje <kowalskicodes@gmail.com>
2022-04-24 22:01:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,23 @@ configureMongo()
 
     app.use(cors());
 
+    app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      next();
+
+      app.options("*", (req, res) => {
+        res.header(
+          "Access-Control-Allow-Methods",
+          "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+        );
+        res.send();
+      });
+    });
+
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
 
